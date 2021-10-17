cf11f47b567f740c234a95ef84583db03c8bbbfd
Hrechykha <88730834+Hrechykha@users.noreply.github.com>
2021-10-17 21:45:48 +0300

    Update settings.py

--- avtocheking/settings.py
+++ avtocheking/settings.py
@@ -129,9 +129,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+    
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
+
+DATABASES = ['default'].update(prod_db)
