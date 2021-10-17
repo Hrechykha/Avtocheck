294633cc96ff51aacb6e51dae9c970061fb2617f
Hrechykha <88730834+Hrechykha@users.noreply.github.com>
2021-10-17 21:57:38 +0300

    Update settings.py

--- avtocheking/settings.py
+++ avtocheking/settings.py
@@ -139,5 +139,10 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 
 
 #DATABASES = {'default'}.update(prod_db)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
