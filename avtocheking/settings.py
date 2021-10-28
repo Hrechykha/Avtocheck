62fd0db57f66912548b2cb3437027895079136dd
viktoria.hrechykha <viktoryia.hrechykha@pandadoc.com>
2021-10-28 20:28:46 +0300

    dbbbb

--- avtocheking/settings.py
+++ avtocheking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,19 +133,13 @@ STATIC_URL = '/static/'
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
     
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
 
-
-#DATABASES = {'default'}.update(prod_db)
+#prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+    'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
+}
+
 
 # Основной url для управления медиафайлами
 MEDIA_URL = '/media/'
